854bf315a38cc4506385c24d906a013c6ff993fe
jilcimar.fernandes <jilcimar.fernandes@lais.huol.ufrn.br>
2020-01-02 15:09:06 -0300

    resto das migrações

---
+++ database/migrations/2020_01_02_180441_create_curriculos_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurriculosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curriculos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->string('email');
+            $table->string('telefone');
+            $table->text('apresentacao');
+            $table->string('linkedin')->nullable();
+            $table->string('github')->nullable();
+            $table->integer('ingles');
+            $table->integer('salario')->nullable();
+            $table->string('anexo');
+
+            $table->integer('vaga_id');
+            $table->foreign('vaga_id')->references('id')->on('vagas')->onDelete('cascade');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curriculos');
+    }
+}
