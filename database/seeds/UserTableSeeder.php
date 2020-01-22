1415878f95afc1df1a317b331ac95db0f339301f
jilcimar.fernandes <jilcimar.fernandes@lais.huol.ufrn.br>
2020-01-22 10:03:24 -0300

    seeder com usuario default

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        /*
+         * Usuário Default
+         */
+        \App\User::insert(
+            [
+                'name' => "Admin",
+                'email' => "admin@admin.com",
+                'password' => bcrypt('123')
+            ]);
+    }
+}
